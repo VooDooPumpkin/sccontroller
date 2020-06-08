3dd2b2c8f3786b76d1809f7334d7f53015c46f3b
VooDooPumpkin <samweanchester@gmail.com>
2020-06-08 15:19:03 +0400

    add endpoints

--- sccontroller/__init__.py
+++ sccontroller/__init__.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+import time
+import copy
+from datetime import timedelta
+from threading import Thread
+from flask import json, abort, request, g
+from flask_api import FlaskAPI, status, exceptions
+from flask_jwt import JWT, jwt_required
+from werkzeug.security import check_password_hash
+from solcx import set_solc_version, get_installed_solc_versions, install_solc
+from .sccontroller import SCContoller
+from .contract import Contract
+from .template import Template
 
 
 def create_app(test_cfg=None):
-
-    app = Flask(__name__, instance_relative_config=True)
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.urandom(16),
+        DATABASE=os.path.join(app.instance_path, 'sccontroller.sqlite'),
+        JWT_EXPIRATION_DELTA=timedelta(hours=1)
     )
 
     if test_cfg is None:
@@ -14,13 +26,213 @@ def create_app(test_cfg=None):
     else:
         app.config.from_mapping(test_cfg)
 
+    if not 'NODE' in app.config:
+        print('Specify eth node address in instance/config.py')
+        exit(-1)
+    if not 'DEFAULT_ACCOUNT' in app.config:
+        print ('Specify default eth account in instance/config.py')
+        exit(-1)
+
+    try:
+        if not 'v0.6.2' in  get_installed_solc_versions():
+            install_solc('v0.6.2')
+            set_solc_version('v0.6.2')
+    except Exception as e:
+        print('Unable to install solc v0.6.2: ' + str(e))
+        exit(-1)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    def authenticate(username, password):
+        user = auth.get_user(username)
+        if user and check_password_hash(user.password, password):
+            return user
+
+    def identity(payload):
+        user_id = payload['identity']
+        return auth.get_user_by_id(user_id)
+
+
+    jwt = JWT(app, authenticate, identity)
+
+    @app.errorhandler(400)
+    def custom400(error):
+        response = json.dumps({'message': error.description['message']})
+        return response
+
+    @app.errorhandler(503)
+    def custom503(error):
+        response = json.dumps({'message': error.description['message']})
+        return response
+
+    @app.route('/templates')
+    @jwt_required()
+    def templates():
+        """Return templates"""
+
+        try:
+            templates = Template.all()
+
+            return [{
+                'id': it.id,
+                'name': it.name,
+                'description': it.description,
+                'parameters_list': [param for param in it.parameters_list if param['name'] != "refund"]
+            } for it in templates], status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/templates/<int:id>')
+    @jwt_required()
+    def template(id):
+        """Return template info by id
+
+        Keyword arguments:
+        id -- id of the template
+        """
+
+        try:
+            template = Template.by_id(id)
+
+            return {
+                'id': template.id,
+                'name': template.name,
+                'description': template.description,
+                'parameters_list': [param for param in template.parameters_list if param['name'] != "refund"]
+            }, status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/create-cotract', methods=['POST'])
+    @jwt_required()
+    def create_contract():
+        """Create and deploy contract from template with params
+
+        Data arguments:
+        template_id -- id of the template
+        parameters -- params dict key=value (keys as keys from templates parameters_list)
+        """
+        try:
+            data = request.get_json(True)
+            scc = SCContoller.get_scc()
+
+            contract = scc.generate(data['template_id'], data['parameters'])
+            if scc.w3.eth.getBalance(scc.w3.eth.defaultAccount) < scc.deploy_gas_cost(contract):
+                abort(status.HTTP_503_SERVICE_UNAVAILABLE, {'message': 'Not enough gas in system to deploy'})
+            scc.deploy(contract)
+
+            return {"id": contract.id}, status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/contracts')
+    @jwt_required()
+    def get_contracts():
+        """Return contracts info"""
+
+        try:
+            contracts = Contract.all()
+
+            return [{
+                'id': contract.id,
+                'template_id': contract.template.id,
+                'address': contract.address,
+                'status': contract.get_status(),
+                'creator_guide': contract.template.creator_guide,
+                'users_guide': contract.template.users_guide,
+            } for contract in contracts], status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/contracts', methods=['POST'])
+    @jwt_required()
+    def get_contracts_by_id():
+        """Return contracts info by ids
+
+        Keyword arguments:
+        ids -- array of ids of the contracts
+        """
+
+        try:
+            data = request.get_json(True)
+
+            contracts = [Contract.by_id(id) for id in data['ids']]
+
+            return [{
+                'id': contract.id,
+                'template_id': contract.template.id,
+                'address': contract.address,
+                'status': contract.get_status(),
+                'creator_guide': contract.template.creator_guide,
+                'users_guide': contract.template.users_guide,
+            } for contract in contracts], status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/contracts/<int:id>')
+    @jwt_required()
+    def get_contract(id):
+        """Return contract info by id
+
+       Keyword arguments:
+       id -- id of the contract
+       """
+
+        try:
+            contract = Contract.by_id(id)
+
+            return {
+                'id': contract.id,
+                'template_id': contract.template.id,
+                'address': contract.address,
+                'status': contract.get_status(),
+                'creator_guide': contract.template.creator_guide,
+                'users_guide': contract.template.users_guide,
+            }, status.HTTP_200_OK
+        except KeyError as e:
+            abort(status.HTTP_400_BAD_REQUEST, {'message': str(e)})
+
+    @app.route('/contracts/<int:id>/destroy')
+    @jwt_required()
+    def destroy_contract(id):
+        """Destroy the contract by id and return success of the destruction
+
+        Keyword arguments:
+        id -- id of the contract
+        """
+        SCContoller.get_scc().destruct(Contract.by_id(id))
+
+        return '', status.HTTP_204_NO_CONTENT
+
+    from . import db, auth, template_controller
+    db.init_app(app)
+    auth.init_app(app)
+    template_controller.init_app(app)
+
+    def handle_event(event):
+        status = event['args']['cur_status']
+        address = event['address']
+        contract = Contract.by_address(address)
+        contract.set_status(status)
+
+    def log_loop(event_filters, poll_interval):
+        with app.app_context():
+            while True:
+                for event_filter in event_filters:
+                    for event in event_filter.get_new_entries():
+                        handle_event(event)
+                time.sleep(poll_interval)
+
+    with app.app_context():
+        try:
+            scc = SCContoller.get_scc()
+            event_filters = [scc.event_filter(template) for template in Template.all()]
+            worker = Thread(target=log_loop, args=(event_filters, 5), daemon=True)
+            worker.start()
+        except:
+            pass
 
     return app
---
+++ sccontroller/sccontroller.py
@@ -0,0 +1,98 @@
+from web3 import Web3
+from solcx import compile_files
+from flask import json, current_app, g
+import os
+from .contract import Contract
+from .template import Template
+
+
+class SCContoller:
+
+    def __init__(self, ganache, defaul_account, root_path):
+        """Constructor"""
+        self.w3 = Web3(Web3.HTTPProvider(ganache))
+        self.w3.eth.defaultAccount = self.w3.toChecksumAddress(defaul_account)
+        self.templates_dir = root_path.replace('\\', '/') + '/templates'
+
+    @staticmethod
+    def get_scc():
+        if 'scc' not in g:
+            g.scc = SCContoller(current_app.config['NODE'], current_app.config['DEFAULT_ACCOUNT'],
+                                current_app.root_path)
+
+        return g.scc
+
+    def generate(self, template_id, parameters):
+        template = Template.by_id(template_id)
+        contract = compile_files([os.path.normpath(self.templates_dir + '/' + template.filename)])
+        identifier = '{}:{}'.format(self.templates_dir + '/' + template.filename, template.contract_name)
+        if 'refund' in [param['name'] for param in template.parameters_list]:
+            parameters['refund'] = self.w3.eth.defaultAccount
+        contract = Contract(template_id, contract[identifier]['bin'], contract[identifier]['abi'], parameters=parameters)
+
+        return contract
+
+    def get_contract(self, id):
+        contract = Contract.by_id(id)
+
+        return contract
+
+    def deploy(self, contract):
+        # Instantiate and deploy contract
+        w3_contract = self.w3.eth.contract(
+            abi=contract.abi,
+            bytecode=contract.binary_code
+        )
+        # Get transaction hash from deployed contract
+        tx_hash = w3_contract.constructor(**contract.parameters).transact()
+        # Get tx receipt to get contract address
+        tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+        contract_address = tx_receipt['contractAddress']
+        contract.set_address(contract_address)
+
+        return contract_address
+
+    def destruct(self, contract):
+        w3_contract = self.w3.eth.contract(address=self.w3.toChecksumAddress(contract.address),
+                                abi=contract.abi)
+
+        tx_hash = w3_contract.functions.kill().transact()
+        receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+
+        Contract.delete(contract.id)
+
+        return receipt
+
+    def transaction_gas_cost(self, transaction):
+        gas_used = self.w3.eth.estimateGas(transaction)
+        gas_price = self.w3.eth.gasPrice
+
+        return gas_price * gas_used
+
+    def deploy_gas_cost(self, contract):
+        params = contract.parameters
+        contract = self.w3.eth.contract(
+            abi=contract.abi,
+            bytecode=contract.binary_code
+        )
+        gas_used = contract.constructor(**params).estimateGas()
+        gas_price = self.w3.eth.gasPrice
+
+        return gas_price * gas_used
+
+    def event_filter(self, template):
+        contract = compile_files([os.path.normpath(self.templates_dir + '/' + template.filename)])
+        identifier = '{}:{}'.format(self.templates_dir + '/' + template.filename, template.contract_name)
+
+        contract = contract.pop(identifier)
+
+        contract = self.w3.eth.contract(
+            abi=contract['abi'],
+            bytecode=contract['bin']
+        )
+
+        event_filter = contract.events.StatusChanged.createFilter(fromBlock="latest")
+
+        return event_filter
+
+
